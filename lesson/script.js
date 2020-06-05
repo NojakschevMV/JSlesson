84717e5f9b6f8079ad128e309cbef7aa063985aa
Mihail Nojakshev <Nojakschev@mail.ru>
2020-06-05 18:13:43 +0500

    Выложил урок номер 2

---
+++ lesson/script.js
@@ -0,0 +1,17 @@
+﻿let money = 36000;
+let income = 'фриланс';
+let addExpenses = 'Интернет, такси, пиво, чипсики';
+let deposit = true;
+let mission = 100000;
+let period = 12;
+let budgetDay = money / 30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
--- lesson01/script.js
+++
@@ -1,8 +0,0 @@
-﻿let money;
-let income;
-let addExpenses; 
-let deposit;
-let mission;
-let period;
-alert('Нажмите "ок"');
-console.log('Спасибо что нажали "ок"');
