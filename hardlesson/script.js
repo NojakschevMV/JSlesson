5b18841311b4a619c567b8e6764d2fb811757c42
Mihail Nojakshev <Nojakschev@mail.ru>
2020-06-05 20:45:34 +0500

    загрузил сложное дз урок 2

---
+++ hardlesson/script.js
@@ -0,0 +1,18 @@
+﻿let num = '266219';
+let summ;
+let iMax = num.length;
+let arr = [];
+for (var i = 0; i < iMax; i++) {
+    arr[i] = num.charAt(i);
+
+}
+console.log(arr);
+summ = 1;
+for (var i = 0; i < iMax; i++) {
+    summ = summ*arr[i];
+
+}
+console.log(summ);
+summ = String(summ**3);
+console.log(summ);
+console.log(summ.substring(0, 2));
