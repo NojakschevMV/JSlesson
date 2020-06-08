463c1e3e8be3bdd61d925c5971f88e5d303daba1
Mihail Nojakshev <Nojakschev@mail.ru>
2020-06-08 17:55:10 +0500

    Загрузил дз к уроку 3

--- lesson/script.js
+++ lesson/script.js
@@ -1,17 +1,41 @@
-﻿let money = 36000;
-let income = 'фриланс';
-let addExpenses = 'Интернет, такси, пиво, чипсики';
-let deposit = true;
+﻿"use strict";
+let money = +prompt("Ваш ежемесячный доход? Введите только число.");
+let income = "фриланс";
+let addExpenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую."
+);
+let deposit = confirm("Есть ли у Вас депозит в банке?");
+let expenses1 = prompt("Введите обязательную статью расходов?");
+let amount1 = +prompt("Во сколько это обойдется?");
+let expenses2 = prompt("Введите обязательную статью расходов?");
+let amount2 = +prompt("Во сколько это обойдется?");
+let budgetMonth = money - amount1 - amount2;
 let mission = 100000;
 let period = 12;
-let budgetDay = money / 30;
+let budgetDay = budgetMonth / 30;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+console.log("Бюджет на месяц " + budgetMonth + " рублей");
+console.log("Бюджет на день " + Math.floor(budgetDay) + " рублей");
+console.log(
+  "Цель будет достигнута за: " +
+    Number(Math.floor(mission / budgetMonth)) +
+    " месяцев"
+);
 console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-console.log(budgetDay);
+console.log(addExpenses.split(", "));
+
+if (budgetDay >= 1200) {
+  console.log("У вас высокий уровень дохода");
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log("У вас средний уровень дохода");
+} else if (budgetDay <= 600 && budgetDay > 0) {
+  console.log("К сожалению у вас уровень дохода ниже среднего");
+} else if (budgetDay < 0) {
+  console.log("Что то пошло не так");
+}
