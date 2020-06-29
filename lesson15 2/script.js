6df15589c021d9f15844b2711aa68c3d4a07fa5f
NojakschevMV <59133169+NojakschevMV@users.noreply.github.com>
2020-06-29 19:35:53 +0500

    Добавил дз к уроку 15

---
+++ lesson15 2/script.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
+  }
+}
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const first = new First();
+const second = new Second();
+
+console.log(first);
+console.log(second);
+second.hello();
